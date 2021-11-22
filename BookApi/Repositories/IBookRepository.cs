cc607ab099f6f821a409003aa8c3d1368daa3df0
Farhan Tanvin <farhan.itsd@goldenharvestbd.com>
2021-11-22 09:57:40 +0600

    final update

---
+++ BookApi/Repositories/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Upadate(Book book);
+        Task Delete(int id);
+
+    }
+}
